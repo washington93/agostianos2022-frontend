97a2c7bf173521adba2287b056cfe465388d4493
p-washington.ribeiro <washington.s@escolar.ifrn.edu.br>
2022-07-23 16:47:43 -0300

    Test QRCode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: () => import('./content/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: 'home', loadChildren: () => import('./content/home/home.module').then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./content/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'home' , loadChildren: () => import('./content/home/home.module').then(m => m.HomeModule) },
   { path: 'login', loadChildren: () => import('./content/login/login.module').then(m => m.LoginModule) }
 ];
 
