fdf3abea44a544f674e83f7a70909a666b543944
p-washington.ribeiro <washington.s@escolar.ifrn.edu.br>
2022-07-23 17:39:35 -0300

    Routes config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./content/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '' , loadChildren: () => import('./content/home/home.module').then(m => m.HomeModule) },
   { path: 'home' , loadChildren: () => import('./content/home/home.module').then(m => m.HomeModule) },
+  { path: 'dashboard', loadChildren: () => import('./content/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'login', loadChildren: () => import('./content/login/login.module').then(m => m.LoginModule) }
 ];
 
