cffc7f3178ff93a91eb7d523adb44735c08e5d3f
washington93 <washington.ribeiro@paneas.com>
2022-07-31 16:56:23 -0300

    update enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://15.228.223.202:3333',
+  apiUrl: 'http://ec2-18-231-184-131.sa-east-1.compute.amazonaws.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3333',
-  apiUrl: 'http://15.228.223.202:3333',
+  apiUrl: 'https://ec2-18-231-184-131.sa-east-1.compute.amazonaws.com:3333',
 };
 
 /*
